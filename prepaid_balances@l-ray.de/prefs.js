6f9f391e7f0d31833109e667f8e764fdeeec3474
l-ray <lennart.reuther+buhtig@gmail.com>
2016-03-17 22:36:59 +0000

    fixes not implemented version-label

--- prepaid_balances@l-ray.de/prefs.js
+++ prepaid_balances@l-ray.de/prefs.js
@@ -164,8 +164,8 @@ const PrepaidOverviewPrefsWidget = new GObject.Class({
                 this.configWidgets.push([theObjects[i], name]);
             }
         }
-        if (Me.metadata.version !== undefined)
-            this.Window.get_object('version').set_label(Me.metadata.version.toString());
+        /*if (Me.metadata.version !== undefined)
+            this.Window.get_object('version').set_label(Me.metadata.version.toString()); */
     },
 
     clearEntry: function() {
